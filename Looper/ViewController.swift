5a90e9bd6314213961691a66d13c13a48076303d
Julian Silvestri <juliansilvestri92@gmail.com>
2021-10-19 20:56:45 -0400

    Initial Commit

---
+++ Looper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Looper
+//
+//  Created by Julian Silvestri on 2021-10-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
